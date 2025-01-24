f3ffe24d33280ad9b18445ee73e48aeb710bcd2c
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2025-01-23 20:07:22 -0500

    package building

--- helloScript.R
+++ helloScript.R
@@ -1,11 +1,13 @@
 # Create a Hello user function in R
 
 # makes a function named hello that takes a variable named name
-hello <- function(name) 
+hello <- function(name)
 {
-  # string output for the function 
+  # string output for the function
   # Hello "input" ???
   paste("Hello ", name, "???", sep = '') # paste has a space separator characters between inputs as a default
 }
 ###
 
+# Edit your description
+usethis::use_mit_license() # generates a liscence
