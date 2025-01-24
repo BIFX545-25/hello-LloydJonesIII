9771316bb34bdf69054c9eed9c5311fe5d372549
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2025-01-23 20:26:07 -0500

    package generation

--- R/helloScript.R
+++ R/helloScript.R
@@ -1,17 +1,22 @@
-# hello.R
-# source code for the `hello::hello()`
+# helloscript.R
+# source code for the `helloscript::hello()`
 
-#' hello
+# Create a Hello user function in R
+#' helloscrpit
 #' A function to say hello
 #'
 #' @param name Name of individual(s) to greet
 #'
 #' @return A character vector of greetings
 #' @export
+# makes a function named hello that takes a variable named name
 hello <- function(name)
 {
   # string output for the function
   # Hello "input" ???
   paste("Hello ", name, "???", sep = '') # paste has a space separator characters between inputs as a default
-
 }
+###
+
+# Edit your description file manually
+## usethis::use_mit_license() # generates a mit style license
