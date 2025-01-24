da57427f8fe5e8a5b2ce551f3fe58a62f54b5897
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2025-01-23 19:31:39 -0500

    Update helloScript.R
    added comment with paste0 rules

--- helloScript.R
+++ helloScript.R
@@ -5,5 +5,5 @@ hello <- function(name)
 {
   # string output for the function 
   # Hello "input" ???
-  paste0("Hello ", name, "???") 
+  paste0("Hello ", name, "???") # paste0 has no separator characters between inputs 
 }
