6b72a3a1074a1770f45701bfbc6a89f97ec50936
Lloyd Jones III <98987194+LloydJonesIII@users.noreply.github.com>
2025-01-23 19:28:05 -0500

    Create helloScript.R

---
+++ helloScript.R
@@ -0,0 +1,9 @@
+# Create a Hello user function in R
+
+# makes a function named hello that takes a variable named name
+hello <- function(name) 
+{
+  # string output for the function 
+  # Hello "input" ???
+  paste0("Hello ", name, "???") 
+}
